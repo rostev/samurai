8b1b236ad04b4d62675a30357153066e5acd076e
Ruprecht <rostevWork@mail.ru>
2021-06-10 16:54:06 +1000

    Lesson 14. Add classes to conponents

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,25 @@
+const Profile = () => {
+    return (
+        <div className="content areas">
+            <div>
+                <img
+                    src="https://vjoy.cc/wp-content/uploads/2019/07/1-1.jpg"
+                    alt="nature"
+                    className="nature"/>
+            </div>
+            <div>
+                Avatar and description
+            </div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+                <div>Post 3</div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Profile;
